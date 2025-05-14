209de9800f07e57a301fc5df2dcc634f3e51d139
sessi123 <zinmonsesylvie@gmail.com>
2025-05-14 17:02:06 +0100

    ...

--- resources/views/intervention/form.blade.php
+++ resources/views/intervention/form.blade.php
@@ -1,7 +1,7 @@
 <div class="">
     <div class="row">
 
-        <div class="col-lg-6 form-group mb-2 mb20">
+        <div class="col-lg-12 form-group mb-2 mb20">
             <strong> <label for="libelle" class="form-label">{{ __('Libelle') }}</label> <!-- <strong class="text-danger"> * </strong> -->  </strong>
             <input type="text" name="libelle" class="form-control @error('libelle') is-invalid @enderror rounded-05" value="{{ old('libelle', $intervention?->libelle) }}" id="libelle" >
             {!! $errors->first('libelle', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
@@ -11,7 +11,7 @@
             <input type="text" name="louerchambre_id" class="form-control @error('louerchambre_id') is-invalid @enderror rounded-05" value="{{ old('louerchambre_id', $intervention?->louerchambre_id) }}" id="louerchambre_id" >
             {!! $errors->first('louerchambre_id', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div> --}}
-        <div class="col-lg-6 form-group mb-2 mb20">
+        <div class="col-lg-12 form-group mb-2 mb20">
             <strong>
                 <label for="description" class="form-label">
                     {{ __('Description') }}
