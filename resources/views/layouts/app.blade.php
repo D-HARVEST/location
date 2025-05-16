56f8bb2c6adbed68c3ae774cf42f6b9796d09a0c
Seth2k21 <baiseth5@gmail.com>
2025-05-16 14:13:22 +0100

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="shortcut icon" type="image/png" href="{{ asset('spike/assets/images/logos/favicon.png') }}" />
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/icone.png') }}" />
 
     <!-- Core Css -->
     <link rel="stylesheet" href="{{ asset('spike/assets/css/styles.css') }}" />
